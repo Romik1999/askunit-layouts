547d85cb4404bb2d10b79292533279cfd625b6b6
Romik1999 <usankin.roma@gmail.com>
2023-03-16 01:26:49 +0400

    try polzunok

--- src/js/main.js
+++ src/js/main.js
@@ -244,72 +244,90 @@ $(function () {
         sel.addClass('active');
     });
 
-    // range
-    const sumInput = document.querySelector('.form__input--range'),
-        rangeInputSum = document.querySelector('.form-range__input'),
-        rangeTrackSum = document.querySelector('.form-range__fill');
+    // // range
+    // const sumInput = document.querySelector('.form__input--range'),
+    //     rangeInputSum = document.querySelector('.form-range__input'),
+    //     rangeTrackSum = document.querySelector('.form-range__fill');
+    //
+    // sumInput.value = sumInput.value.replace(/[^0-9]/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ' ') + ' руб'
+    //
+    // // маска
+    // function prettify(num) {
+    //     const n = num.toString();
+    //     return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ') + ' руб';
+    // }
+    //
+    // function range(input$, progress$, content) {
+    //     if (input$) {
+    //         const val = input$.value;
+    //         const min = input$.getAttribute('min');
+    //         const max = input$.getAttribute('max');
+    //         const step = input$.getAttribute('step');
+    //         const position = 100 / (max - step) * (val - step);
+    //         updateRangePosition(progress$, position);
+    //
+    //         input$.addEventListener('input', () => {
+    //             const val = input$.value;
+    //             const min = input$.getAttribute('min');
+    //             const max = input$.getAttribute('max');
+    //             const step = input$.getAttribute('step');
+    //             const position = 100 / (max - step) * (val - step);
+    //             updateRangePosition(progress$, position);
+    //             content.value = prettify(val);
+    //         });
+    //     }
+    // }
+    //
+    // function updateRangePosition(progress$, position) {
+    //     if (progress$) {
+    //         progress$.style.width = `${position}%`;
+    //     }
+    // }
+    //
+    // range(rangeInputSum, rangeTrackSum, sumInput);
+    //
+    //
+    // sumInput.addEventListener('input', function () {
+    //
+    //     const minSum = rangeInputSum.getAttribute('min');
+    //     const maxSum = rangeInputSum.getAttribute('max');
+    //     const stepSum = rangeInputSum.getAttribute('step');
+    //
+    //     this.value = prettify(this.value.replace(/\D/g, ''))
+    //     if (+this.value.replace(/\D/g, '') > +maxSum) {
+    //         this.value = prettify(maxSum)
+    //         return
+    //     }
+    //     if (+this.value.replace(/\D/g, '') < +minSum) {
+    //         rangeInputSum.value = 0
+    //         rangeTrackSum.style.width = 0 + '%'
+    //         return
+    //     }
+    //     if (+this.value.replace(/\D/g, '') >= +minSum && +this.value.replace(/\D/g, '') <= +maxSum) {
+    //         rangeTrackSum.style.width = `${100 / (maxSum - stepSum) * (this.value.replace(/\D/g, '') - stepSum)}%`;
+    //         rangeInputSum.value = this.value.replace(/\D/g, '')
+    //     }
+    // })
 
-    sumInput.value = sumInput.value.replace(/[^0-9]/g, '').replace(/\B(?=(\d{3})+(?!\d))/g, ' ') + ' руб'
+    // // Example functionality to demonstrate a value feedback
+    // function valueOutput(element) {
+    //     var value = element.value,
+    //         output = element.parentNode.getElementsByTagName('output')[0];
+    //     output.innerHTML = value;
+    // }
+    // for (var i = $('input[type="range"]').length - 1; i >= 0; i--) {
+    //     valueOutput($('input[type="range"]')[i]);
+    // };
+    // $(document).on('input', 'input[type="range"]', function(e) {
+    //     valueOutput(e.target);
+    // });
+    // // end
 
-    // маска
-    function prettify(num) {
-        const n = num.toString();
-        return n.replace(/(\d{1,3}(?=(?:\d\d\d)+(?!\d)))/g, "$1" + ' ') + ' руб';
-    }
-
-    function range(input$, progress$, content) {
-        if (input$) {
-            const val = input$.value;
-            const min = input$.getAttribute('min');
-            const max = input$.getAttribute('max');
-            const step = input$.getAttribute('step');
-            const position = 100 / (max - step) * (val - step);
-            updateRangePosition(progress$, position);
-
-            input$.addEventListener('input', () => {
-                const val = input$.value;
-                const min = input$.getAttribute('min');
-                const max = input$.getAttribute('max');
-                const step = input$.getAttribute('step');
-                const position = 100 / (max - step) * (val - step);
-                updateRangePosition(progress$, position);
-                content.value = prettify(val);
-            });
-        }
-    }
-
-    function updateRangePosition(progress$, position) {
-        if (progress$) {
-            progress$.style.width = `${position}%`;
-        }
-    }
-
-    range(rangeInputSum, rangeTrackSum, sumInput);
-
-
-    sumInput.addEventListener('input', function () {
-
-        const minSum = rangeInputSum.getAttribute('min');
-        const maxSum = rangeInputSum.getAttribute('max');
-        const stepSum = rangeInputSum.getAttribute('step');
-
-        this.value = prettify(this.value.replace(/\D/g, ''))
-        if (+this.value.replace(/\D/g, '') > +maxSum) {
-            this.value = prettify(maxSum)
-            return
-        }
-        if (+this.value.replace(/\D/g, '') < +minSum) {
-            rangeInputSum.value = 0
-            rangeTrackSum.style.width = 0 + '%'
-            return
-        }
-        if (+this.value.replace(/\D/g, '') >= +minSum && +this.value.replace(/\D/g, '') <= +maxSum) {
-            rangeTrackSum.style.width = `${100 / (maxSum - stepSum) * (this.value.replace(/\D/g, '') - stepSum)}%`;
-            rangeInputSum.value = this.value.replace(/\D/g, '')
-        }
-    })
-
-    $('[name*="phone"]').inputmask({
-        mask: '+7 999 999 99 99'
+    $('input[type="range"]').rangeslider({
+        polyfill: false
     });
+
+    // $('[name*="phone"]').inputmask({
+    //     mask: '+7 999 999 99 99'
+    // });
 });
